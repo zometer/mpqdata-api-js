49182868800db3cdb8ace69f20e0cccea0f1c492
Chris Spears <zometer@gmail.com>
2022-03-09 12:37:05 -0600

    refactoring remote config loading into its own module

--- config/index.js
+++ config/index.js
@@ -1,25 +1,10 @@
-const fetch = require('sync-fetch');
-const YAML = require('yaml');
-const token = process.env.GITHUB_API_TOKEN; 
-const configUrl = process.env.CONFIG_URL;
+const db = require('./db');
+const remoteConfig = require('./remoteConfig');
 
-const config = {
-  db: {
-    url: process.env.DB_URL,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD
-  }
-};
+const config = {db, ...remoteConfig};
 
-const remoteYaml = fetch(configUrl, {
-  headers: { 
-    "Accept": "application/vnd.github.v3.raw", 
-    "Authorization": `bearer ${token}`
-  }
-}).text(); 
-
-const remoteConfig = YAML.parse(remoteYaml); 
-Object.assign(config, remoteConfig);
 config.remoteApis.mpq.deviceId = process.env.MPQ_API_DEVICEID;
+
+console.log("configuration loaded\n", config);
 
 module.exports = config;
