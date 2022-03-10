c4b82f0c184b18fe1e98ac07bb2cbb4fc5e59099
Chris Spears <zometer@gmail.com>
2022-03-10 14:49:02 -0600

    Adding error handler for rest endpoints

--- web/app.js
+++ web/app.js
@@ -4,4 +4,15 @@ const applyRoutes = require('../routes');
 
 applyRoutes(app);
 
+/* Error Handling */
+app.use('/api/rest/*', (error, req, res, next) => { 
+  const err = {error: error.name, message: error.message}; 
+  if (process.env.NODE_ENV !== 'production') { 
+    err.stack = error.stack; 
+  }
+  console.error(error);
+  res.status(500);
+  res.send(err);
+});
+
 module.exports = app; 
