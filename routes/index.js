b93bca6ff732962ae854a3680ad08ff15d724b54
Chris Spears <zometer@gmail.com>
2022-03-10 15:54:36 -0600

    Adding alliance controller and route endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
+const allianceRoutes = require('./allianceRoutes');
 const coverRoutes = require('./coverRoutes');
 const helloRoutes = require('./helloRoute');
 
 const applyRoutes = (app) => {
   helloRoutes(app);
   coverRoutes(app);
+  allianceRoutes(app);
 }
 
 module.exports = applyRoutes;
