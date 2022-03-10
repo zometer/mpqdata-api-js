c82bd03104e00bced5bec90fffa9847e741dedd0
Chris Spears <zometer@gmail.com>
2022-03-10 14:49:35 -0600

    Adding Alliance domain class

---
+++ model/Alliance.js
@@ -0,0 +1,15 @@
+class Alliance { 
+  constructor(guid, name, type, maxSize, size) { 
+    this.allianceGuid = guid;
+    this.allianceName = name; 
+    this.allianceType = type;
+    this.allianceMaxSize = maxSize; 
+    this.members = [];
+  }
+
+  get allianceSize () { 
+    return this.members.length;
+  }
+}
+
+module.exports = Alliance;
