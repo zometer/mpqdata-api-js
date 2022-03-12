32ad946c2e3fd70f9e89fd40041e8e2e57703843
Chris Spears <zometer@gmail.com>
2022-03-11 19:14:15 -0600

    Adding AbilityLevel model

---
+++ model/AbilityLevel.js
@@ -0,0 +1,31 @@
+const DataTypes = require('sequelize');
+const sequelize = require('./sequelize');
+
+const AbilityLevel = sequelize.define('AbilityLevel', {
+  abilityId: {
+    type: DataTypes.INTEGER, 
+    primaryKey: true, 
+    autoIncrementIdentity: true
+  }, 
+  abilityKey: DataTypes.STRING, 
+  mpqCharacterKey: DataTypes.STRING, 
+  abilitySet: DataTypes.INTEGER, 
+  ordinalPosition: DataTypes.INTEGER,
+  color: DataTypes.STRING, 
+  abilityLevel: { 
+    type: DataTypes.VIRTUAL, 
+    get() { 
+      return this.getDataValue('abilityLevel');
+    }, 
+    set(value) { 
+      this.setDataValue('abilityLevel', value); 
+    }
+  }
+}, {
+  schema: "mpq_data", 
+  tableName: "display_ability_level_vw", 
+  timestamps: false,
+  underscored: true
+}); 
+
+module.exports = AbilityLevel; 
