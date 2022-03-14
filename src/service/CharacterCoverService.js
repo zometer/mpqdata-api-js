1fe48cc95bff7a8a3285816abab185a6e8845169
Chris Spears <zometer@gmail.com>
2022-03-13 22:26:31 -0500

    Adding GcdApiClient

--- src/service/CharacterCoverService.js
+++ src/service/CharacterCoverService.js
@@ -1,6 +1,7 @@
 const CharacterCover = require('../model/CharacterCover');
 const logger = require('log4js').getLogger('CharacterCoverService');
 const MarvelApiClient = require('../remoteApis/MarvelApiClient');
+const GcdApiClient = require('../remoteApis/GcdApiClient');
 
 const CharacterCoverService = {
   fetchAll: async () => await CharacterCover.findAll(),
@@ -8,14 +9,21 @@ const CharacterCoverService = {
   fetchUnprocessed: async () => await CharacterCover.findAll( { where: { complete: false } } ),
   searchCoverImages: async (searchParams) => {
     logger.debug('searchParams', searchParams);
-    const results = searchMarvelCovers(searchParams);
-    return results;
+    const results = await searchMarvelCovers(searchParams);
+    const gcdResults = await searchGcdCovers(searchParams);
+    return results.concat(gcdResults);
   }
 };
 
 const searchMarvelCovers = async (params) => {
   const results = await MarvelApiClient.queryForImages(params);
-  logger.debug('response', results);
+  logger.debug('MarvelApiClient.response', results);
+  return results;
+};
+
+const searchGcdCovers = async (params) => {
+  const results = await GcdApiClient.queryForImages(params);
+  logger.debug('GcdApiClient.response', results);
   return results;
 };
 
