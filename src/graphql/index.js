3e63779b376f85d4f6e00c65848668036b2c5e07
Chris Spears <zometer@gmail.com>
2022-03-18 16:11:39 -0500

    Adding unprocessedCovers() graphql query

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -3,6 +3,7 @@ const { buildSchema } = require('graphql');
 const { hello } = require('../service/HelloService');
 const AllianceService = require('../service/AllianceService');
 const PlayerService = require('../service/PlayerService');
+const CharacterCoverService = require('../service/CharacterCoverService');
 
 const schemaContent = readFileSync('./src/graphql/schema.graphql', 'utf-8');
 const schema = buildSchema(schemaContent);
@@ -13,7 +14,8 @@ const resolver = {
   allianceByGuid: ({ guid }) => AllianceService.fetchByGuid(guid),
   searchAlliances: ({ name, includeFull, includePrivate }) => AllianceService.search(name, includeFull, includePrivate),
   playerByName: ({ name }) => PlayerService.fetchByDisplayPlayerByName(name),
-  rosteredCharacter: ({ name, instanceId }) => PlayerService.fetchRosteredCharacter(name, instanceId)
+  rosteredCharacter: ({ name, instanceId }) => PlayerService.fetchRosteredCharacter(name, instanceId),
+  unprocessedCovers: () => CharacterCoverService.fetchUnprocessed()
 };
 
 const graphqlOptions = {
